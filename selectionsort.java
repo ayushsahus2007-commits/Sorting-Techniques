df5795cd718f134c62d28e6c864cf182aff673d1
Ayush Sahu <ayushsahus2007@gmail.com>
2025-09-17 22:47:02 +0530

    Defining function

--- selectionsort.java
+++ selectionsort.java
@@ -1,23 +1,24 @@
 public class selectionsort{
-    public static void main(String[] arg){
-       int []a={2,1,3,4,7,5,6};
-       int n=a.length;
-       for(int i=0;i<=n-1;i++){
+    static void sort(int ...a){
+        int n=a.length;
+        for(int i=0;i<=n-1;i++){
         int m=i;
         for(int j=i;j<=n-1;j++){
             if(a[j]<a[m]){
                 m=j;
-                int c;
-                c=a[j];
-                a[j]=a[i];
-                a[i]=c;
             }
         }
+                int c;
+                c=a[m];
+                a[m]=a[i];
+                a[i]=c;
        }
        for(int i=0;i<n;i++){
         System.out.print(a[i]+" ");
        }
     }
-
-
+    public static void main(String[] arg){
+        int []arr={4,3,1,5,2};
+        sort(arr);
+    }
 }
